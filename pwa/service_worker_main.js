33639ceae2575c345fcadf5f8140c78ba417a998
The Doggy Brad <94173621+thedoggybrad@users.noreply.github.com>
2022-12-23 20:40:34 +0800

    Forgotten link

--- pwa/service_worker_main.js
+++ pwa/service_worker_main.js
@@ -11,7 +11,8 @@ const resourceList = [
   'https://thedoggybrad.github.io/text2speech/wave.gif',
   'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
   'https://code.jquery.com/jquery-3.3.1.slim.min.js',
-  'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js'
+  'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js'
 
 ];
 
